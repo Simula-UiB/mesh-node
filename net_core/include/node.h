73865c8967a7f10700b984095701733d609a9676
Stian <stian108@gmail.com>
2021-02-04 15:00:59 +0100

    Message struct..

--- net_core/include/node.h
+++ net_core/include/node.h
@@ -1,7 +1,12 @@
 #include <stdint.h>
 
+struct radio_msg {
+    size_t len;
+    uint8_t * data;
+};
+
 void init_void();
 
-void handle_receive(uint8_t * data, uint8_t length);
+void handle_receive(struct radio_msg msg);
 
 void process_packet();
