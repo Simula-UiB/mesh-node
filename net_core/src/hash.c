36ab11aa8b2ccedf676b1af2f4e18c271c836bc6
Stian <stian108@gmail.com>
2021-02-25 13:14:21 +0100

    Fixed hash

--- net_core/src/hash.c
+++ net_core/src/hash.c
@@ -24,24 +24,22 @@ uint32_t hash_djb2(uint8_t *bytes, size_t len)
 
 uint32_t hash_packet(struct ipc_msg msg)
 {
-
     uint32_t hash = 5381;
 
-    // TODO This crashes, find out why
-    // for (size_t i = 0; i < 6; i++)
-    // {
-    //     hash += (hash << 5) + msg.data[ORIGINAL_SRC_MAC_POS + i];
-    // }
-    // for (size_t i = 0; i < 6; i++)
-    // {
-    //     hash += (hash << 5) + msg.data[DST_MAC_POS + i];
-    // }
-    // hash += (hash << 5) + msg.data[MSG_NUMBER_POS];
-    // hash += (hash << 5) + msg.data[PAYLOAD_LENGTH_POS];
-    // for (size_t i = DATA_POS; i < msg.len; i++)
-    // {
-    //     hash += (hash << 5) + msg.data[i];
-    // }
+    for (size_t i = 0; i < 6; i++)
+    {
+        hash += (hash << 5) + msg.data[ORIGINAL_SRC_MAC_POS + i];
+    }
+    for (size_t i = 0; i < 6; i++)
+    {
+        hash += (hash << 5) + msg.data[DST_MAC_POS + i];
+    }
+    hash += (hash << 5) + msg.data[MSG_NUMBER_POS];
+    hash += (hash << 5) + msg.data[PAYLOAD_LENGTH_POS];
+    for (size_t i = DATA_POS; i < msg.len; i++)
+    {
+        hash += (hash << 5) + msg.data[i];
+    }
 
     return hash;
 }
