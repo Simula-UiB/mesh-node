d06767c57bb5787863880df7dfa90c4778eea53f
Stian <stian108@gmail.com>
2021-04-29 14:30:05 +0200

    Added empty example.

---
+++ examples/empty/src/main.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+#include <stdio.h>
+
+#include <common.h>
+#include <mesh.h>
+
+void main(void)
+{
+    init_mesh();
+}
+
+void mesh_receive(uint8_t *data, size_t len, uint8_t *src, bool broadcast)
+{
+}
